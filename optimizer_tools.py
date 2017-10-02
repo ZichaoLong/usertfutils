0367ff55ba61a109b105024f65e9ae8cd8157bca
zlong <zlong@pku.edu.cn>
2017-10-02 14:01:22 +0800

    增加updater类更新变量;增加constraint utils处理约束优化
    原来的objfunc_wrapper中成员函数'update'改由updater处理
    cosntraint utils目前支持变量逐个元素等式约束

--- optimizer_tools.py
+++ optimizer_tools.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 #%%
 import tensorflow as tf
+import functools
 from numpy import *
-from functools import reduce
 from . import precision_control
-__all__ = ['objfunc_wrapper', 'array2list', 'list2array']
+__all__ = ['objfunc_wrapper', 'array2list', 'list2array', 'updater', 'xopt_check', 'grad_check', 'constraint_objfunc']
 #%%
 def array2list(x, variables_size, variables_shape, iscopy=True):
     N = int(sum(variables_size))
@@ -27,8 +27,34 @@ def list2array(l, variables_size, variables_shape):
         x[i:i+variables_size[j]] = reshape(l[j], (variables_size[j],))
         i += variables_size[j]
     return x
-
-# optimize utils
+#%% updater
+class updater(object):
+    def __init__(self, fathter_updater=None, registered_vars=None):
+        if fathter_updater is None:
+            self.placeholder = {}
+            self.assignment_op = {}
+        else:
+            self.placeholder = dict((v,fathter_updater.placeholder[v]) for v in registered_vars)
+            self.assignment_op = dict((v,fathter_updater.assignment_op[v]) for v in registered_vars)
+    def add(self, *tensors):
+        for v in tensors:
+            w = tf.placeholder(shape=v.get_shape(), dtype=v.dtype)
+            self.placeholder[v] = w
+            self.assignment_op[v] = tf.assign(v, w)
+        return None
+    def update(self, feed_dict=None, sess=None):
+        """
+        feed_dict.keys(): 需要更新的tensors
+        feed_dict.values(): 对应的values
+        """
+        sess = (tf.get_default_session() if sess is None else sess)
+        feed_dict = ({} if feed_dict is None else feed_dict)
+        for v in feed_dict.keys():
+            assert v in self.placeholder
+        feed_dict_placeholder = dict((self.placeholder[v],feed_dict[v]) for v in feed_dict.keys())
+        sess.run(list(self.assignment_op[v] for v in feed_dict.keys()), feed_dict=feed_dict_placeholder)
+        return None
+#%% optimize utils
 def tfloss_meta(loss):
     trainable_vars = tf.trainable_variables()
     grads_and_vars = zip(tf.gradients(loss, trainable_vars), trainable_vars)
@@ -42,29 +68,13 @@ def tfloss_meta(loss):
     variables_shape = []
     for v in variables:
         s = tuple(map(int, v.get_shape()))
-        variables_size.append((1 if len(s) == 0 else reduce(multiply, s)))
+        variables_size.append((1 if len(s) == 0 else functools.reduce(multiply, s)))
         variables_shape.append(s)
     variables_num = len(variables)
     allsize = int(sum(variables_size))
     with tf.name_scope('array2tensors/'+loss.name.split(':')[0]):
-        array2tensors_placeholder = []
-        array2tensors_assignop = []
-        for j in range(variables_num):
-            array2tensors_placeholder.append(
-                    tf.placeholder(
-                        dtype=precision_control.TENSOR_PRECISION(),
-                        shape=variables_shape[j],
-                        name='placeholder/'+variables[j].name.split(':')[0]
-                        )
-                    )
-            array2tensors_assignop.append(
-                    tf.assign(
-                        variables[j],
-                        array2tensors_placeholder[j],
-                        name='assign/'+variables[j].name.split(':')[0]
-                        )
-                    )
-        #array2tensors_group_op = tf.group(*array2tensors_assignop, name='array2tensors')
+        variables_updater = updater()
+        variables_updater.add(*variables)
     def tfloss_info():
         pass
     tfloss_info()
@@ -73,24 +83,24 @@ def tfloss_meta(loss):
     tfloss_info.variables_grads = variables_grads
     tfloss_info.variables_size = variables_size
     tfloss_info.variables_shape = variables_shape
-    tfloss_info.array2tensors_placeholder = array2tensors_placeholder
-    tfloss_info.array2tensors_assignop = array2tensors_assignop
+    tfloss_info.variables_updater = variables_updater
     tfloss_info.loss = loss
     tfloss_info.xopt_list = array2list(zeros((allsize,)), variables_size, variables_shape, iscopy=False)
     return tfloss_info
-#%%
+
 class objfunc_wrapper_class(object):
     """
     objfunc_wrapper_class
     几个主要成员变量：
-    loss: tensorflow compute graph中目标函数loss function所代表的tensor
-    variables: 一个由tensorflow variables组成的list
-    variables_grads: 最顶层loss function对variables中各变量梯度的tensor
-    xopt_list: list，由objfunc_wrapper_class实例所持有的variables取值, 类比于sess.run(variables)
+        loss: tensorflow compute graph中目标函数loss function所代表的tensor
+        variables: 一个由tensorflow variables组成的list
+        variables_grads: 最顶层loss function对variables中各变量梯度的tensor
+        xopt_list: list，variables values暂存值；sess.run(variables)为variables当前值
+        xopt_dict: dict, 暂存值的dict版, 即dict(zip(variables, xopt_list))
     其他辅助成员变量:
-    allsize: variables中各变量size之和
-    variables_size,variables_shape: variables中各变量size及shape
-    array2tensors_placeholder, array2tensors_assignop: 用于更新variables
+        allsize: variables中各变量size之和
+        variables_size,variables_shape: variables中各变量size及shape
+        variables_updater: 用于更新variables
     __init__:
         输入: 
         father_wrapper: 可以是一个objfunc_wrapper_class实例，也可由上述tfloss_meta生成
@@ -98,11 +108,11 @@ class objfunc_wrapper_class(object):
             'variables', 'variables_grads', 
             'loss', 'xopt_list' 
             'allsize', 'variables_size', 'variables_shape', 
-            'array2tensors_placeholder', 'array2tensors_assignop', 
+            'variables_updater'
         registered_vars: list, father_wrapper.variables的子集, 
             初始化即执行self.variables = registered_vars并将self的成员、成员函数更新
         xopt_list: 可选，若xopt_list == None, 则self.xopt_list各元素
-            按registered_vars对应继承自father_wrapper.xopt_list
+            按registered_vars对应copy自father_wrapper.xopt_list
     """
     def __init__(self, father_wrapper, registered_vars, xopt_list=None):
         assert isinstance(registered_vars, list)
@@ -116,16 +126,13 @@ class objfunc_wrapper_class(object):
         self.variables_grads = []
         self.variables_size = []
         self.variables_shape = []
-        self.array2tensors_placeholder = []
-        self.array2tensors_assignop = []
+        self.variables_updater = updater(father_wrapper.variables_updater, registered_vars)
         self.xopt_list = []
         for i in registered_vars_indx:
             self.variables.append(father_wrapper.variables[i])
             self.variables_grads.append(father_wrapper.variables_grads[i]) 
             self.variables_size.append(father_wrapper.variables_size[i])
             self.variables_shape.append(father_wrapper.variables_shape[i])
-            self.array2tensors_placeholder.append(father_wrapper.array2tensors_placeholder[i])
-            self.array2tensors_assignop.append(father_wrapper.array2tensors_assignop[i])
             self.xopt_list.append(father_wrapper.xopt_list[i].copy())
         self.xopt_list = (xopt_list if not xopt_list is None else self.xopt_list)
         self.xopt_dict = dict(zip(self.variables, self.xopt_list))
@@ -133,29 +140,27 @@ class objfunc_wrapper_class(object):
         self.variables_num = len(registered_vars)
     def update(self, feed_dict=None, sess=None):
         """
-        feed_dict是一个dict，feed_dict.keys()包含于self.variables, 
-        依据feed_dict更新self.variables中部分变量的值.
-        feed_dict缺省时，使用self.xopt_list更新self.variables
+        依据feed_dict更新指定variables,
+        feed_dict缺省时，使用暂存版self.xopt_list更新self.variables
         """
         feed_dict = (dict(zip(self.variables, self.xopt_list)) if feed_dict is None else feed_dict)
-        assignop_feed_dict = {}
-        assignop = []
-        for v in feed_dict.keys():
-            assert v in self.variables
-            i = self.variables.index(v)
-            assignop.append(self.array2tensors_assignop[i])
-            assignop_feed_dict[self.array2tensors_placeholder[i]] = feed_dict[v]
-        sess = (tf.get_default_session() if sess is None else sess)
-        sess.run(assignop, feed_dict=assignop_feed_dict)
+        self.variables_updater.update(feed_dict, sess)
         return None
-    def get_xopt(self):
-        xopt_dict = {}
-        for v in self.variables:
-            xopt_dict[v] = v.eval()
-        return xopt_dict
+    def get_xopt(self, dtype='dict', sess=None):
+        """
+        返回variables当前值dtype版
+        """
+        sess = (tf.get_default_session() if sess is None else sess)
+        xopt_list = sess.run(self.variables)
+        if dtype == 'list':
+            return xopt_list
+        elif dtype == 'dict':
+            return dict(zip(self.variables, xopt_list))
+        else:
+            return self.l2a(xopt_list)
     def set_xopt(self, xopt, iscopy=True):
         """
-        利用xopt更新self.xopt_list
+        利用xopt更新暂存区self.xopt_list
         其中:xopt可以是
             a ndarray with shape of [self.allsize,],
             or a list of ndarrays as xopt_list,
@@ -182,15 +187,18 @@ class objfunc_wrapper_class(object):
     def l2d(self, xopt_list):
         return dict(zip(self.variables, xopt_list))
     def d2l(self, xopt_dict, iscopy=True):
+        """
+        xopt_dict.keys()包含于(可以不相等)self.variables,
+        返回完整xopt_list, xopt_dict没有的地方用暂存区self.xopt_list补齐
+        """
         xopt_list = []
         for v in self.variables:
             if v in xopt_dict:
                 x = (xopt_dict[v].copy() if iscopy else xopt_dict[v])
-                xopt_list.append(x)
             else:
                 i = self.variables.index(v)
                 x = (self.xopt_list[i].copy() if iscopy else self.xopt_list[i])
-                xopt_list.append(x)
+            xopt_list.append(x)
         return xopt_list
     def a2d(self, xopt, iscopy=True):
         return self.l2d(self.a2l(xopt, iscopy=iscopy))
@@ -235,8 +243,26 @@ def objfunc_wrapper(father_wrapper, registered_vars=None, xopt_list=None):
         registered_vars = father_wrapper.variables
     return objfunc_wrapper_class(father_wrapper, registered_vars, xopt_list)
 objfunc_wrapper.__doc__ = objfunc_wrapper_class.__doc__
+#%% constraint utils
+def constraint_check(x, obj ,constraint, checktype='xopt', iscopy=True):
+    x_dict = obj.a2d(x, iscopy=iscopy)
+    for v in constraint.keys():
+        if v in x_dict:
+            m = constraint[v]
+            x_dict[v][~isnan(m)] = (m[~isnan(m)] if checktype == 'xopt' else 0)
+    if iscopy:
+        return obj.d2a(x_dict)
+    else:
+        return x
+xopt_check = functools.partial(constraint_check, checktype='xopt')
+grad_check = functools.partial(constraint_check, checktype='grad')
+def constraint_objfunc(obj, constraint):
+    def F(x, **kw):
+        return obj.f(xopt_check(x, obj, constraint, iscopy=True), **kw)
+    def G(x, **kw):
+        return grad_check(obj.g(xopt_check(x, obj, constraint, iscopy=True), **kw), obj, constraint, iscopy=False)
+    return F, G
 #%%
-
 def test():
     """
     use tfloss_meta to solve the ls_l2 and ls_l1 system, with lbfgs optimizer: 
